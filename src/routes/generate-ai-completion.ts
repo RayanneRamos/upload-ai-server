da9616bb080be5f5c443a0e8dc5add031acf053e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-14 16:27:24 -0300

    aguardando o texto

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import { z } from "zod";
 import { openai } from "../lib/openai";
+import { streamToResponse, OpenAIStream } from "ai";
 
 export async function generateAiCompletionRoute(app: FastifyInstance) {
   app.post("/ai/complete", async (request, reply) => {
@@ -34,8 +35,16 @@ export async function generateAiCompletionRoute(app: FastifyInstance) {
       model: "gpt-3.5-turbo-16k",
       temperature,
       messages: [{ role: "user", content: promptMessage }],
+      stream: true,
     });
 
-    return response;
+    const stream = OpenAIStream(response);
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      },
+    });
   });
 }
